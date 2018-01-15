74261c19548213276c7924ad1298bd74c1ca9264
Ellen <ellenmerchant@gmail.com>
2018-01-15 19:23:06 +0000

    Updated CV text.

--- resources/views/cv.blade.php
+++ resources/views/cv.blade.php
@@ -2,107 +2,100 @@
 
 @section('content')
     <div class="cv-section">
-        <div class="cv-title">PERSONAL STATEMENT</div>
+        <div class="cv-title">Personal Statement</div>
         In 2010 I finished my degree in Computing gaining a 2:1 with Honours. Since I graduated I have been working for
-        a
-        Telecommunications company as part of the Software Development team. I was first brought on-board as a web
-        developer
-        to utilise my skills in languages such as PHP, MySQL, HTML, CSS and JavaScript. However I was keen to expand my
-        abilities to other areas which included learning new skills such as networking and Android mobile development. I
-        feel I am a big part of making the team successful, this includes developing a custom PHP MVC framework which
-        vastly
-        improved productivity and I also feel I was instrumental in training a strong team of developers. I believe my
-        assets also include time management, problem solving, being self-motivated and dependable.
+        a Telecommunications company as part of the Development team. I was first brought on-board as a web developer to
+        utilise my skills in languages such as PHP, MySQL, HTML, CSS and JavaScript. However I was keen to expand my
+        abilities to other areas which included learning new skills in the DevOps role. I feel I am a big part of making
+        the team successful, this includes implementing the Laravel framework, which has vastly improved productivity
+        and I also feel I was instrumental in training a strong team of developers. I recently got promoted to Business
+        Continuity Manager where I have been responsible for passing the ISO 22301 accreditation. I believe my assets
+        also include time management, problem solving, being self-motivated and dependable.
     </div>
     <div class="cv-section">
-        <div class="cv-title">SPECIALIST SKILLS</div>
-        PHP (7 years)<br>
-        MySQL (7 years)<br>
-        HTML (7 years)<br>
-        CSS (7 years)<br>
-        Linux (5 years)<br>
-        Networking (5 years)
+        <div class="cv-title">Specialist Skills</div>
+        <ul>
+            <li>
+                {{ Carbon\CarbonInterval::year(\Carbon\Carbon::now()->diffInYears(\Carbon\Carbon::createFromDate("2008", "10"))) }}
+                : PHP 5/PHP 7, MySQL, HTML, CSS/SaSS
+            </li>
+            <li>{{ Carbon\CarbonInterval::year(\Carbon\Carbon::now()->diffInYears(\Carbon\Carbon::createFromDate("2010", "06"))) }}
+                years: Linux/LAMP Stack, JavaScript/JQuery/VueJS, Networking
+            </li>
+            <li>{{ Carbon\CarbonInterval::year(\Carbon\Carbon::now()->diffInYears(\Carbon\Carbon::createFromDate("2015", "01"))) }}
+                years: Laravel, Git
+            </li>
+            <li>{{ Carbon\CarbonInterval::year(\Carbon\Carbon::now()->diffInYears(\Carbon\Carbon::createFromDate("2015", "01"))) }}
+                year: PHPUnit
+            </li>
+        </ul>
     </div>
     <div class="cv-section">
-        <div class="cv-title">EMPLOYMENT HISTORY</div>
+        <div class="cv-title">Employment History</div>
         <div class="cv-employ">
             <div class="cv-employ-title">Mainstream Digital - Cirencester, Gloucestershire</div>
-            <div class="cv-employ-subtitle">IT Developer, June 2010 to present</div>
+            <div class="cv-employ-subtitle">Developer ( June 2010 to present), Business Continuity Manager ( June 2017
+                to present)
+            </div>
             During my time at Mainstream I have gained a lot of experience creating high quality public and internal web
-            portals. Most of the systems I developed involved automating procedures which were previously done manually
-            and
-            developing new ideas and products to sell to our customers. My job also involves managing a large workload
-            and
-            leading other members of the team. It also includes designing the systems from the beginning, this includes
-            database design and normalisation, writing specifications and tasks and designing the interface.
+            portals as well as developing backend micro services and RESTful APIs. I have developed various systems
+            while working at Mainstream, including developing new products to sell to our customers and automating in
+            house procedures.
             <br>
-            Within the first year of working at the company I had designed, developed, maintained and enhanced the
-            company’s
-            Intranet which improved the day to day working of the company for all members of staff. I designed the
-            Intranet
-            to allow web portals to be added to it and implemented multi-layered permissions to allow some users
-            functionality
-            that was hidden from others. One of many web portals on the company Intranet I developed was a ticket system
-            which
-            logs customer faults and enquiries. Since its creation the Intranet has become an integral part of the
-            company and as
-            a result could no longer function efficiently without it.
+            My job also involves managing a large workload and leading other members of the team, which gave me
+            increased responsibility. In addition, I also design the systems using an Agile project lifecycle, this
+            includes database design, designing the front end and writing specifications and requirements.
+            Within the first year of working at the company I had established the company’s Intranet, which improved the
+            day to day working of the company for all members of staff. More recently I have developed a public web
+            portal for Mainstream customers where they configure and monitor the different products and services they
+            have with the company.
             <br>
-            While working at Mainstream I also developed skills in networking and server maintenance. This included
-            building
-            Linux servers, installing the programs needed from the CLI and being responsible for connectivity, security
-            and
-            performance of these servers. When I first joined the team we were responsible for maintaining two servers
-            and
-            now we control over one hundred.
+            I have also enhanced my skills in DevOps and networking, which involved deploying and maintaining Linux
+            CentOS servers with XenServer. With my expanded role I became instrumental in ensuring connectivity,
+            security and performance of these servers.
+            Since learning and implementing the Laravel framework three years ago, I feel my skills in Object Orientated
+            coding and test driven development (TDD) have greatly improved. I have also enhanced my skills over the last
+            three years in VueJS, Gulp and SaSS.
+            In recent months I have been training in Android programming. I now add and enhance features on an existing
+            Android application. This shows I have the ability to adapt to new challenges and that I have a strong
+            programming foundation.
             <br>
-            I also spent time developing an Android mobile application which involved tracking a phone’s location and
-            uploading the data to a central server. Once the data had reached the server the locations were displayed on
-            a web portal.
-            As I had never produced an Android application before this was a great learning experience.
-            <br>
-            Recently I have developed a public web portal for Mainstream customers where they configure and monitor the
-            different products and services they have with the company.
+            Last year I earned the role of Business Continuity Manager, which involved gaining the standard of ISO
+            22301:2012 in Business Continuity. I achieved this standard by using my vast knowledge of the company and
+            the underlying infrastructure. This new role involved identifying risks, developing new policies and
+            procedures and then implementing them across the entire company. This shows an ability to manage a large
+            workload as well as being able to interact with personnel outside my own team.
         </div>
         <div class="cv-employ">
             <div class="cv-employ-title">Axiell - Ferndown, Dorset</div>
-            <div class="cv-employ-subtitle">Web Developer, June 2008 to June 2009</div>
+            <div class="cv-employ-subtitle">Web Developer (June 2008 to June 2009)</div>
             The job at Axiell was part of my University placement year. They initially contracted me for 40 weeks which
-            was
-            then extended to 52 weeks after the company acknowledged the high quality work I was producing. My role at
-            the
-            company was to work on a project with over 15 web developers from 4 countries in Europe. My core duty was to
-            create well
-            designed maintainable interface using HTML and CSS. The project also required frequent validation,
-            accessibility
-            and usability testing, which I volunteered to take on and ultimately became my responsibility. Working for
-            the
-            company involved managing a large workload, good communication and team work in addition to excellent
-            technical skills
-            in HTML and CSS.
+            was then extended to 52 weeks after the company acknowledged the high quality work I was producing. My core
+            duty was to create well designed maintainable interface using HTML and CSS. The project also required
+            frequent validation, accessibility and usability testing, which I volunteered to take on and ultimately
+            became my responsibility. Working for the company involved managing a large workload, good communication and
+            team work in addition to excellent technical skills in HTML and CSS.
         </div>
         <div class="cv-employ">
             <div class="cv-employ-title">Pall - Ilfracombe, Devon</div>
-            <div class="cv-employ-subtitle">Line Worker, Mar. 2006 to Sept. 2006</div>
+            <div class="cv-employ-subtitle">Line Worker (Mar. 2006 to Sept. 2006)</div>
             This job involved working to daily targets and working within regulations as well as correctly filling in
             paperwork and forms correctly and efficiently. The skills I obtained from this job were communication, team
-            work and time
-            management.
+            work and time management.
         </div>
         <div class="cv-employ">
             <div class="cv-employ-title">Combe Martin Wildlife Park - Combe Martin, Devon</div>
-            <div class="cv-employ-subtitle">Shop Assistant, Apr. 2003 to Nov. 2005</div>
+            <div class="cv-employ-subtitle">Shop Assistant (Apr. 2003 to Nov. 2005)</div>
             While still in full time education I worked as a shop assistant. The skills I gained working within this
-            role
-            was time management, communication, money handling, customer service and team work.
+            role was time management, communication, money handling, customer service and team work.
         </div>
     </div>
     <div class="cv-section">
-        <div class="cv-title">EDUCATION</div>
+        <div class="cv-title">Education</div>
         <div class="cv-employ">
             <div class="cv-employ-title">Bournemouth University - Bournemouth, Dorset</div>
             <div class="cv-employ-subtitle">2:1 BSc (Hons) Computing</div>
-            Final Year Modules:<br>
+            <u>Final Year Modules:</u><br>
             Web Systems<br>
             Software Systems Modelling<br>
             Digital Media and Games<br>
@@ -122,7 +115,7 @@
         </div>
     </div>
     <div class="cv-section">
-        <div class="cv-title">REFERENCES</div>
+        <div class="cv-title">References</div>
         References are available upon request.
     </div>
     <div class="button-container">
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,11 +7,14 @@
         I specialise in PHP, MySQL, HTML, CSS and JavaScript and have experience in developing using the
         <a href="https://laravel.com">Laravel</a> framework.
         I have been employed as a full time Developer for over
-        {{ \Carbon\Carbon::now('Y')->diffInYears(\Carbon\Carbon::createFromDate("2010", "06")) }} years.
-        <br><br>
+        {{ \Carbon\Carbon::now()->diffInYears(\Carbon\Carbon::createFromDate("2010", "06")) }} years.
+        <br>
         My hobbies include walking, photography, film and TV.
+        <br><br>
+        Here is my GitHub for your perusal:
+        <a target="_blank" href="https://github.com/ellllllen">https://github.com/ellllllen</a>
     </div>
-    <a href="https://github.com/ellllllen">https://github.com/ellllllen</a>
+    <br><br>
     <div class="button-container">
         <a href="{{ route('cv') }}">View my full CV</a>
     </div>
