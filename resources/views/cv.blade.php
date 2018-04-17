2928d27a09602fe27a903ca6651a554cc0ccaed9
Ellen <ellenmerchant@gmail.com>
2018-04-17 14:34:31 +0100

    Deployed on hosting platform.

--- resources/views/cv.blade.php
+++ resources/views/cv.blade.php
@@ -4,13 +4,12 @@
     <div class="cv-section">
         <div class="cv-title">Personal Statement</div>
         In 2010 I finished my degree in Computing gaining a 2:1 with Honours. Since I graduated I have been working for
-        a Telecommunications company as part of the Development team. I was first brought on-board as a web developer to
+        a Telecommunications company as part of the Development team. I was first brought on-board as a Developer to
         utilise my skills in languages such as PHP, MySQL, HTML, CSS and JavaScript. However I was keen to expand my
         abilities to other areas which included learning new skills in the DevOps role. I feel I am a big part of making
-        the team successful, this includes implementing the Laravel framework, which has vastly improved productivity
-        and I also feel I was instrumental in training a strong team of developers. I recently got promoted to Business
-        Continuity Manager where I have been responsible for passing the ISO 22301 accreditation. I believe my assets
-        also include problem solving, mentoring, time management and being reliable.
+        the team successful, this includes implementing the Laravel framework, which has vastly improved productivity. I
+        recently got promoted to Software Team Lead, where I manage, mentor and train a small team of Software
+        Developers to ensure software is produced at a high quality using best practices.
     </div>
     <div class="cv-section">
         <div class="cv-title">Specialist Skills</div>
@@ -34,18 +33,18 @@
         <div class="cv-title">Employment History</div>
         <div class="cv-employ">
             <div class="cv-employ-title">Mainstream Digital - Cirencester, Gloucestershire</div>
-            <div class="cv-employ-subtitle">Developer (June 2010 to present), Business Continuity Manager (June 2017
-                to present)
+            <div class="cv-employ-subtitle">Developer (June 2010 to March 2018), Business Continuity Manager (June 2017
+                to present), Software Team Lead (March 2018 to present)
             </div>
             During my time at Mainstream I have gained a lot of experience creating high quality public and internal web
             portals as well as developing backend micro services and RESTful APIs. I have developed various systems
             while working at Mainstream, including developing new products to sell to our customers and automating in
-            house procedures. Within the first year of working at the company I had established the company’s Intranet,
-            which improved the day to day working for all members of staff. More recently I have developed a public web
-            portal for Mainstream customers where they configure and monitor the different products and services they
-            have with the company.
+            house procedures. Within the first year of working at the Mainstream Digital I had established the company’s
+            Intranet, which improved the day to day working for all members of staff. More recently I have developed a
+            public web portal for Mainstream customers where they configure and monitor the different products and
+            services they have with the company.
             <br>
-            Although the company was fully established when I joined, my team was treated as a start up. Therefore, I
+            Although Mainstream was fully established when I joined, my team was treated as a start up. Therefore, I
             have been able to influence high level decisions such as programming techniques and tools and vastly
             expanding the company data centre infrastructure, showing I am motivated and proactive. My role also
             involves managing projects and mentoring other members of the team, which has given me increased
@@ -53,8 +52,8 @@
             <br>
             Since learning and implementing the Laravel framework three years ago, I feel my skills in Object Orientated
             coding and test driven development (TDD) have greatly improved. I have also enhanced my skills over the last
-            three years in VueJS, Gulp and SaSS. Additionally, I have established my skills in DevOps and networking,
-            which involved deploying and maintaining Linux CentOS servers with XenServer. With my expanded role I became
+            three years in VueJS and SaSS. Additionally, I have established my skills in DevOps and networking, which
+            involved deploying and maintaining Linux CentOS servers with XenServer. With my expanded role I became
             instrumental in ensuring connectivity, security and performance of these servers and the underlying data
             centres.
             <br>
@@ -62,12 +61,15 @@
             Android application. This shows I have the ability to adapt to new challenges and that I have a strong
             programming foundation.
             <br>
-            Last year I earned the role of Business Continuity Manager, which involved gaining the standard of ISO
-            22301:2012 in Business Continuity. I achieved this standard by using my vast knowledge of the company and
-            the underlying infrastructure. This new role involved identifying risks, developing new policies and
+            Last year I earned the role of Business Continuity Manager, this amounts to gaining the ISO 22301:2012 in
+            Business Continuity accreditation. This role involves identifying risks, developing new policies and
             procedures and then implementing them across the entire company. This shows an ability to multi-task as well
-            as being detail orientated.
-
+            as organised and detail oriented.
+            <br>
+            After being promoted to Software Team Lead I have implemented several improvements within the team,
+            including adopting the Scrum methodology, assuring SOLID principles are executed and prioritising projects
+            and tasks. I additionally ensure software is delivered at a high standard within tight deadlines, while
+            encouraging team members to keep abreast of the latest best practices and technologies.
         </div>
         <div class="cv-employ">
             <div class="cv-employ-title">Axiell - Ferndown, Dorset</div>
