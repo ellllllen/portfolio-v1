9432311588d14b960d890af3b38e38cffe52cbae
Ellen <ellenmerchant@gmail.com>
2018-09-05 18:31:10 +0100

    Updating activities

--- resources/lang/en/activities.php
+++ resources/lang/en/activities.php
@@ -2,6 +2,21 @@
 
 return [
     [
+        'title' => 'Continuous Integration',
+        'description' => 'Now I\'ve lots of automated tests I am now researching and implementing continuous integration and deployment (CI and CD).',
+        'startDate' => \Carbon\Carbon::createFromDate(2018, 9, 01),
+        'titleLink' => 'http://ellenm.me/articles/10',
+        'display' => 1
+    ],
+    [
+        'title' => 'Automated Testing',
+        'description' => 'Researching and implementing automated testing. So writing loads unit,
+            feature and browser testing for this website.',
+        'startDate' => \Carbon\Carbon::createFromDate(2018, 07, 21),
+        'titleLink' => 'https://github.com/ellllllen/personal-website',
+        'display' => 1
+    ],
+    [
         'title' => 'Scalable Microservices with Kubernetes',
         'description' => 'Free Udacity Course, learning to containerise applications with Docker and Kubernetes.',
         'startDate' => \Carbon\Carbon::createFromDate(2018, 07, 10),
@@ -10,8 +25,7 @@ return [
     ],
     [
         'title' => 'Chatbot',
-        'description' => 'Started following a tutorial on how to build a chatbot in PHP using Laravel and Botman.
-                Check out my Github to follow my progress.',
+        'description' => 'Started following a tutorial on how to build a chatbot in PHP using Laravel and Botman.',
         'startDate' => \Carbon\Carbon::createFromDate(2018, 07, 06),
         'titleLink' => 'https://github.com/ellllllen/chatbot',
         'display' => 1
