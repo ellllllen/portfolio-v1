606457284f9ee7c944652f506e7b549d5f60f37e
Ellen Merchant <ellenmerchant@gmail.com>
2020-04-05 14:44:44 +0100

    Fix build

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,7 +26,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(UrlGenerator $url)
     {
-        if(env('APP_ENV') !== 'local') {
+        if(env('APP_ENV') !== 'local' && env('APP_ENV') !== 'testing') {
             $url->forceScheme('https');
         }
 
